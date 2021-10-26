1a8c10460eaa09eb4bdeb66ced05a2b69db32653
liujie <13698006215@163.com>
2021-10-26 20:22:33 +0800

    改进为线程池爬虫

--- craw_detail.py
+++ craw_detail.py
@@ -1,47 +1,63 @@
 # 爬取商品标题及评论
 import os
 import re
-import random
-import time
 import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from proxy import get_html, get_random_ua
 from settings import logger
 
+
+pool = ThreadPoolExecutor(max_workers=6)
 
 # 响应头及url
 comment_url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&productId={}&score=0&sortType=5&page={}&pageSize=10&isShadowSku=0&rid=0&fold=1'
 
 
+# 请求和解析函数
+# id为商品id, i为页数
+def request_and_parse(id, i):
+    return_comments = []
+    try:
+        logger.info("爬取商品id:{}评论\t第{}页".format(id, i))
+        text = get_html(comment_url.format(id, i), headers=get_random_ua())
+        res = re.findall('fetchJSON_comment98\((.*?)\);', text, re.S)
+        comments = json.loads(res[0], strict=False)
+        for comment in comments['comments']:
+            # logger.info(comment['content'])
+            return_comments.append(comment['content'].strip())
+        return return_comments
+    except:
+        logger.info("爬取商品id:{}评论\t第{}页出错".format(id, i))
+        return return_comments
+
+
 # 获取商品标题及评论
 @logger.catch
 def get_title_comments(id):
-    return_comments = []
+    all_return_comments = []
 
     # 爬取标题
-    logger.info("爬取id:{}的商品".format(id))
+    logger.info("爬取id:{}的商品评论".format(id))
     # 爬取评论
-    for i in range(30):
-        try:
-            # logger.info(f'开始id:{id}\t爬虫第{i}页评论')
-            text = get_html(comment_url.format(id, i), headers=get_random_ua())
-            res = re.findall('fetchJSON_comment98\((.*?)\);', text, re.S)
-            comments = json.loads(res[0], strict=False)
-            for comment in comments['comments']:
-                # logger.info(comment['content'])
-                return_comments.append(comment['content'].strip())
-            time.sleep(random.random(2,6)/10)
-        except Exception as e:
-            logger.error(f'id:{id}\t爬虫第{i}页评论发生异常')
-            continue
+    future_list = []
+    for i in range(40):
+        future = pool.submit(request_and_parse, id, i)
+        future_list.append(future)
 
-    return return_comments
+        for return_comments_obj in as_completed(future_list):
+            return_comments = return_comments_obj.result()
+            if len(return_comments) > 0:
+                all_return_comments.extend(return_comments)
+
+    return all_return_comments
 
 
 # 保存数据
 def store_data(category, id, title, return_comments):
-    if return_comments is None or len(return_comments) == 0:
+    if return_comments is None or len(return_comments) == 0 or len(return_comments) < 200:
         return
 
+    return_comments = list(set(return_comments))
     if not os.path.exists(f'./{category}'):
         os.mkdir(f'./{category}')
 
@@ -60,7 +76,7 @@ def crawl_by_category():
         category = category_file_name.split('.')[0]
         category_file_name = os.path.join('./category', category_file_name)
         with open(category_file_name, 'r', encoding='utf-8') as fr:
-            for line in fr.readlines():
+            for line in fr.readlines()[:1]:
                 items = line.strip().split('\t')
                 if len(items) == 2:
                     id, title = items[0], items[1]
--- list_spider.py
+++ list_spider.py
@@ -1,11 +1,12 @@
 # 爬虫列表页
 import re
-import time
-import random
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from lxml import etree
 from proxy import get_html, get_random_ua
 from settings import logger
 
+
+pool = ThreadPoolExecutor(max_workers=4)
 
 # 判断评论数是否合格
 pattern = re.compile('(\\d+万+)|(\\d{3,}\\+)')
@@ -23,6 +24,24 @@ def load_name_url():
         return name_url_list
 
 
+# 请求和解析函数
+def request_and_parse(url, category, i):
+    try:
+        logger.info("爬取分类:{}\t第{}页".format(category, i))
+        text = get_html(url.format(i), headers=get_random_ua())
+        html_tree = etree.HTML(text)
+        product_list = html_tree.xpath('//li[@data-sku]/div[@class="gl-i-wrap"]')  # 获取商品列表
+        id_title_list = []
+        for product in product_list:
+            id = product.xpath('./div[@class="p-name p-name-type-3"]/a/i/@id')[0].split('_')[-1]
+            title = ''.join(product.xpath('./div[@class="p-name p-name-type-3"]/a/em/text()'))
+            id_title_list.append((id, title))
+        return id_title_list
+    except:
+        logger.info("爬取分类:{}\t第{}页出错".format(category, i))
+        return None
+
+
 # 爬取每一个分类
 def crawl_by_category(name_url):
     # 默认爬取100页
@@ -31,25 +50,23 @@ def crawl_by_category(name_url):
     logger.info("开始爬取分类:{}".format(name))
     url += '&psort=4&page={}'
     with open('./category/{}.txt'.format(name), 'w', encoding='utf-8') as fw:
+        future_list = []
         for i in range(1, 100):
-            try:
-                text = get_html(url.format(i), headers=get_random_ua())
-                time.sleep(random.randint(2,6)/10)
-                logger.info("爬取分类:{}\t第{}页".format(name, i))
-                html_tree = etree.HTML(text)
-                product_list = html_tree.xpath('//li[@data-sku]/div[@class="gl-i-wrap"]')   # 获取商品列表
-                for product in product_list:
-                    id = product.xpath('./div[@class="p-name p-name-type-3"]/a/i/@id')[0].split('_')[-1]
-                    title = ''.join(product.xpath('./div[@class="p-name p-name-type-3"]/a/em/text()'))
-                    if id is not None and len(id.strip()) > 0:
-                        fw.write('{}\t{}\n'.format(id.strip(), title.strip()))
-                        # logger.info('{}\t{}'.format(id.strip(), title.strip()))
-                    else:
-                        logger.info("错误数据id:{}\ttitle:{}".format(id, title))
-                fw.flush()
-            except Exception as e:
-                print(e)
-                continue
+            future = pool.submit(request_and_parse, url, name, i)
+            future_list.append(future)
+
+        all_id_title_list = []
+        for id_title_list_future in as_completed(future_list):
+            id_title_list = id_title_list_future.result()
+            if id_title_list is not None:
+                all_id_title_list.extend(id_title_list)
+
+        index = 0
+        for id_, title in all_id_title_list:
+            if id_ is None or title is None:
+                logger.info("错误数据index:{}".format(index))
+            else:
+                fw.write('{}\t{}\n'.format(id_.strip(), title.strip()))
 
 
 if __name__ == '__main__':
